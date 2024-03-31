cf9699876bd974f61cfc5826af38a24a6ddf72e0
Philip Alexander Hassialis <philip.alexander.hassialis@gmail.com>
2024-03-31 18:58:14 +0300

    User input

--- profit_calculator.go
+++ profit_calculator.go
@@ -1,5 +1,18 @@
 package main
 
+import "fmt"
+
 func main() {
+
+	var revenue, expenses, taxRate float64
+
+	fmt.Print("Enter revenue:")
+	fmt.Scan(revenue)
+
+	fmt.Print("Enter expenses:")
+	fmt.Scan(expenses)
+
+	fmt.Print("Enter tax rate:")
+	fmt.Scan(taxRate)
 
 }
